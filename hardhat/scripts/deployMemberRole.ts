e7fcc08d998adcfbf450473503b2371faeb6a3fa
Benedek Robert George <72063093+911-Benedek-RobertGeorge@users.noreply.github.com>
2023-01-08 16:59:41 +0200

    deploy the contract on local node

---
+++ hardhat/scripts/deployMemberRole.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+	const memberRoleFactory = await ethers.getContractFactory("MemberRole");
+
+	const memberRole = await memberRoleFactory.deploy();
+
+	await memberRole.deployed(); /// just waiting for the block to be included in a block
+
+	console.log(`MemberRole deployed to ${memberRole.address} `);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
